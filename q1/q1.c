18b137cf2812abf6ed44cdd7ac66c51bcd2845de
quote023 <acs2@ecomp.poli.br>
2022-09-21 14:41:14 -0300

    q1 e q5

---
+++ q1/q1.c
@@ -0,0 +1,68 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct somar_props
+{
+  int id;
+  int qtd_candidatos;
+  int *votos;
+};
+
+pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *somar(void *args)
+{
+  struct somar_props *props = (struct somar_props *)args;
+  int x = 0;
+  char nomeArquivo[6];
+  sprintf(nomeArquivo, "%d.in", props->id);
+
+  FILE *file = fopen(nomeArquivo, "r");
+  while (!feof(file))
+  {
+    fscanf(file, "%d", &x);
+    while (pthread_mutex_trylock(&mymutex))
+      ;
+    ++(props->votos[x]);
+    pthread_mutex_unlock(&mymutex);
+  }
+
+  return 0;
+}
+
+int setup(int qtd_candidatos, int qtd_threads)
+{
+  pthread_t ts[qtd_threads];
+  int votos[qtd_candidatos + 1];
+  int total = 0;
+  int vencedor = 0;
+
+  memset(votos, 0, sizeof(votos));
+
+  for (int i = 0; i < qtd_threads; ++i)
+    pthread_create(&ts[i], NULL, somar, &(struct somar_props){.id = i, .qtd_candidatos = qtd_candidatos, .votos = votos});
+  for (int i = 0; i < qtd_threads; ++i)
+    pthread_join(ts[i], NULL);
+
+  for (int i = 0; i <= qtd_candidatos; ++i)
+    total += votos[i];
+
+  for (int i = 1; i <= qtd_candidatos; ++i)
+  {
+    if (votos[vencedor] < votos[i])
+      vencedor = i;
+    printf("Candidato %d:\tVotos: %d\t(%d%%)\n", i, votos[i], ((votos[i] * 100) / total));
+  }
+
+  printf("\nVotos Nulos: %d\nTotal de Votos: %d\n", votos[0], total);
+  printf("O Candidato %d venceu as eleicoes!\n", vencedor);
+
+  return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+  return setup(3, 3);
+}
---
+++ q5/q5.c
@@ -0,0 +1,74 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// função sleep
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#define FILOSOFOS_COUNT 5
+
+pthread_t filosofos[FILOSOFOS_COUNT];
+pthread_mutex_t garfos[FILOSOFOS_COUNT];
+
+int think(int n)
+{
+  printf("\nFilosofo %d esta pensando...", n);
+  return 0;
+}
+
+int get_forks(int n)
+{
+  pthread_mutex_lock(&garfos[n]);
+  pthread_mutex_lock(&garfos[(n + 1) % 5]);
+  return 0;
+}
+
+int eat(int n)
+{
+  printf("\nFilosofo %d esta comendo!", n);
+  sleep(n + 1);
+  return 0;
+}
+
+int put_forks(int n)
+{
+  pthread_mutex_unlock(&garfos[n]);
+  pthread_mutex_unlock(&garfos[(n + 1) % 5]);
+  return 0;
+}
+
+void *filosofo(int n)
+{
+  while (1)
+  {
+    think(n);
+    get_forks(n);
+    eat(n);
+    put_forks(n);
+    printf("\nFilosofo %d terminou de comer!", n);
+  }
+
+  return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+  // Inicializar mutexes
+  for (int i = 0; i < FILOSOFOS_COUNT; i++)
+    pthread_mutex_init(&garfos[i], NULL);
+
+  // Inicializar threads
+  for (int i = 0; i < FILOSOFOS_COUNT; i++)
+    pthread_create(&filosofos[i], NULL, (void *)filosofo, (void *)i);
+
+  // Aguarda finalizar
+  for (int i = 0; i < FILOSOFOS_COUNT; i++)
+    pthread_join(filosofos[i], NULL);
+
+  pthread_exit(NULL);
+  return 0;
+}
